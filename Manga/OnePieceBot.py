d48e8d38d6dcd95571c2f46736a48bd66e2d6a95
root <root@DESKTOP-0NE3HIF.localdomain>
2022-04-17 09:14:41 +0300

    add some function to CCBot.py

--- Manga/OnePieceBot.py
+++ Manga/OnePieceBot.py
@@ -98,7 +98,7 @@ def chapter(update: Update, context: CallbackContext) -> None :
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater(token='ENTER YOUR TOKEN', use_context=True)
+    updater = Updater(token='397823070:AAFAnRP2RwlpU_TC_QFnzhociPceODt6eus', use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
